f3792273d2862a7d4c9efc04298611f59647c737
lluuksz30 <lluuksz30@gmail.com>
2024-05-17 11:29:42 +0200

    Move code from app to LevelView package

--- Sources/LevelView/LevelView.swift
+++ Sources/LevelView/LevelView.swift
@@ -1,2 +1,36 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct LevelView: View {
+    let level: Int
+    let maxLevel: Int
+    let color: Color
+    let accessibilityLabel: String
+    
+    var body: some View {
+        HStack {
+            ForEach(0 ..< level, id: \.self) { number in
+                Image(systemName: "circle.fill")
+                    .foregroundStyle(color)
+            }
+            
+            ForEach(0 ..< maxLevel - level, id: \.self) { number in
+                Image(systemName: "circle")
+                    .foregroundStyle(color)
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityIdentifier("Level")
+        .accessibilityLabel(Text(accessibilityLabel))
+    }
+}
+
+#Preview {
+    LevelView(
+        level: 3,
+        maxLevel: 5,
+        color: .red,
+        accessibilityLabel: "3/5")
+}
