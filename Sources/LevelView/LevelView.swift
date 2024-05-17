32c9f698b70d9d49ea316830c7db83c3e18ae451
lluuksz30 <lluuksz30@gmail.com>
2024-05-17 19:30:39 +0200

    Remove color and accessibilityLabel parameters

--- Sources/LevelView/LevelView.swift
+++ Sources/LevelView/LevelView.swift
@@ -8,45 +8,30 @@ import SwiftUI
 /// - Parameters:
 ///   - level: The require level.
 ///   - maxLevel: The maximum level.
-///   - color: The color of dots.
-///   - accessibilityLabel: The accessbility label.
 ///
 public struct LevelView: View {
     private let level: Int
     private let maxLevel: Int
-    private let color: Color
-    private let accessibilityLabel: String
     
-    public init(level: Int, maxLevel: Int, color: Color, accessibilityLabel: String) {
+    public init(level: Int, maxLevel: Int) {
         self.level = level
         self.maxLevel = maxLevel
-        self.color = color
-        self.accessibilityLabel = accessibilityLabel
     }
     
     public var body: some View {
         HStack {
             ForEach(0 ..< level, id: \.self) { number in
                 Image(systemName: "circle.fill")
-                    .foregroundStyle(color)
             }
             
             ForEach(0 ..< maxLevel - level, id: \.self) { number in
                 Image(systemName: "circle")
-                    .foregroundStyle(color)
             }
-            
         }
-        .accessibilityElement(children: .ignore)
-        .accessibilityIdentifier("Level")
-        .accessibilityLabel(Text(accessibilityLabel))
     }
 }
 
 #Preview {
-    LevelView(
-        level: 3,
-        maxLevel: 5,
-        color: .red,
-        accessibilityLabel: "3/5")
+    LevelView(level: 3, maxLevel: 5)
+        .foregroundStyle(.red)
 }
